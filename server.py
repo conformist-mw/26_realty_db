2d97c44148cdcb3f02ea062a324a41360051364a
conformist-mw <conformist.mw@gmail.com>
2017-01-10 00:27:49 +0200

    implement db filters according to task

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from sqlalchemy.sql import func
 from models import db, Ad
 
 
@@ -7,6 +8,7 @@ def create_app():
     app.config.from_object('config')
     db.init_app(app)
     return app
+
 
 app = create_app()
 
@@ -27,6 +29,20 @@ def ads_list():
         "premise_area": 43.0,
     }] * 10
     )
+
+
+@app.route('/search', methods=['POST'])
+def search():
+    max_num = db.session.query(func.max(Ad.price)).one()[0]
+    new_building = request.form.get('new_building', False)
+    oblast_district = request.form['oblast_district']
+    min_price = request.form.get('min_price', 0, type=int)
+    max_price = request.form.get('max_price', max_num, type=int)
+    ans = db.session.query(Ad).filter(
+        Ad.oblast_district == oblast_district,
+        Ad.new_building.is_(bool(new_building)),
+        Ad.price > min_price, Ad.price < max_price).all()
+    return render_template('ads_list.html', ads=ans)
 
 
 if __name__ == "__main__":
