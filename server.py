e69a6b06f2e2d84974af2d660b29e9ece0e09163
conformist-mw <conformist.mw@gmail.com>
2017-01-09 12:20:51 +0200

    implement create_app func to send contenx in other files

--- server.py
+++ server.py
@@ -1,23 +1,33 @@
 from flask import Flask, render_template
+from models import db, Ad
 
-app = Flask(__name__)
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('config')
+    db.init_app(app)
+    return app
+
+app = create_app()
+
 
 @app.route('/')
 def ads_list():
     return render_template('ads_list.html', ads=[{
-            "settlement": "Череповец",
-            "under_construction": False,
-            "description": '''Квартира в отличном состоянии. Заезжай и живи!''',
-            "price": 2080000,
-            "oblast_district": "Череповецкий район",
-            "living_area": 17.3,
-            "has_balcony": True,
-            "address": "Юбилейная",
-            "construction_year": 2001,
-            "rooms_number": 2,
-            "premise_area": 43.0,
-        }]*10
+        "settlement": "Череповец",
+        "under_construction": False,
+        "description": '''Квартира в отличном состоянии. Заезжай и живи!''',
+        "price": 2080000,
+        "oblast_district": "Череповецкий район",
+        "living_area": 17.3,
+        "has_balcony": True,
+        "address": "Юбилейная",
+        "construction_year": 2001,
+        "rooms_number": 2,
+        "premise_area": 43.0,
+    }] * 10
     )
 
+
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0')
