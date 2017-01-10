d45f2bc99a278325aab5cb999e5b5d12144a363d
conformist-mw <conformist.mw@gmail.com>
2017-01-10 23:57:06 +0200

    split search route to search and result

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for
 from sqlalchemy.sql import func
 from models import db, Ad
 
@@ -6,6 +6,7 @@ from models import db, Ad
 def create_app():
     app = Flask(__name__)
     app.config.from_object('config')
+    app.secret_key = '!secret'
     db.init_app(app)
     return app
 
@@ -21,18 +22,27 @@ def ads_list(page=1):
 
 
 @app.route('/search', methods=['POST'])
-@app.route('/search/<int:page>', methods=['GET', 'POST'])
 def search(page=1):
     max_num = db.session.query(func.max(Ad.price)).one()[0]
-    new_building = request.form.get('new_building', False, type=bool)
     oblast_district = request.form['oblast_district']
     min_price = request.form.get('min_price', 0, type=int)
     max_price = request.form.get('max_price', max_num, type=int)
+    new_building = request.form.get('new_building', False, type=bool)
+    session['dist'] = oblast_district
+    session['new'] = new_building
+    session['minv'] = min_price
+    session['maxv'] = max_price
+    return redirect(url_for('results'))
+
+
+@app.route('/results', methods=['GET'])
+@app.route('/results/<int:page>', methods=['GET'])
+def results(page=1):
     ads = Ad.query.filter(
-        Ad.oblast_district == oblast_district,
-        Ad.new_building.is_(new_building),
-        Ad.price > min_price, Ad.price < max_price,
-        Ad.active.is_(True)).order_by(Ad.price).paginate(page, 10)
+        Ad.oblast_district == session['dist'],
+        Ad.new_building.is_(session['new']),
+        Ad.price > session['minv'], Ad.price < session['maxv'],
+        Ad.active.is_(True)).order_by(Ad.price).paginate(page, 10, False)
     return render_template('ads_list.html', ads=ads)
 
 
